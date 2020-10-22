bfc110893ef45e5ebd28a98d32a15babb2f5203f
Magdalena Fernlund <magfer-3@student.ltu.se>
2020-10-22 18:11:01 +0200

    Lagt in en tabell och två dropdown, men ingen funktionalitet.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Our D0031N app"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Registrera studieresultat"/>
   </div>
 </template>
 
