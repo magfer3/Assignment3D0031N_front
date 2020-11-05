877a652af605b554114d08f363c62dc7fa202abb
Magdalena Fernlund <magfer-3@student.ltu.se>
2020-11-05 15:43:23 +0100

    Uppdaterat tabellen, går att bocka i rader, och fylla i info för datum och betyg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,8 +23,36 @@
       <!-- tabellen-->
       <!-- itererar nu över items-arrayen i data()-->
       <!-- ska uppdateras till lista med studenter och omdöme i Canvas efter att kurskoden är vald-->
-      <b-table striped hover :items="items" :fields="fields"></b-table>
-    
+      <b-table
+      ref="selectableTable"
+      selectable
+      :select-mode="selectMode"
+      :items="items"
+      :fields="fields"
+      @row-selected="onRowSelected"
+      responsive="sm"
+    >
+      <!-- Example scoped slot for select state illustrative purposes -->
+      <!--<template #cell(selected)="{ rowSelected }">
+        <template v-if="rowSelected" >
+          <span aria-hidden="true">&check;</span>
+          <span class="sr-only">Selected</span>
+        </template>
+        <template v-else>
+          <span aria-hidden="true">&nbsp;</span>
+          <span class="sr-only">Not selected</span>
+        </template>
+      </template>
+      -->
+      <template v-slot:cell(examinationsdatum)="row">
+        <b-form-input v-model="row.item.examinationsdatum"/>
+      </template>
+            <template v-slot:cell(ladok_grade)="row">
+        <b-form-select v-model="row.item.ladok_grade" :options="grades_options"></b-form-select>
+      </template>
+    </b-table>
+
+    {{selected_items}}
   </div>
 
 </template>
@@ -39,11 +67,20 @@ export default {
   },
   data() {
       return {
-        fields: ['firstName', 'lastName', 'grades' ],
+        fields: [ 
+          {key: 'firstName', label: 'Förnamn'}, 
+          {key: 'lastName', label: 'Efternamn'},
+          {key: 'grades', label: 'Betyg'},
+          {key: 'examinationsdatum'},
+          {key: 'ladok_grade'},
+          {key: 'Status'},
+          {key: 'Information'}],
         items: [{"firstName":"Lukas","grades":[{"grade":"U","nameOfAssignment":"Inlämningsuppgift1"},{"grade":"G","nameOfAssignment":"Inlämningsuppgift2"},{"grade":"U","nameOfAssignment":"Inlämningsuppgift3"},{"grade":"VG","nameOfAssignment":"Projekt"}],"lastName":"Skog","userName":"luksok-8"},{"firstName":"Magdalena","grades":[{"grade":"VG","nameOfAssignment":"Inlämningsuppgift1"},{"grade":"VG","nameOfAssignment":"Inlämningsuppgift2"},{"grade":"G","nameOfAssignment":"Inlämningsuppgift3"},{"grade":"VG","nameOfAssignment":"Projekt"}],"lastName":"Fernlund","userName":"magrad-2"},{"firstName":"Simon","grades":[{"grade":"G","nameOfAssignment":"Inlämningsuppgift1"},{"grade":"G","nameOfAssignment":"Inlämningsuppgift2"},{"grade":"VG","nameOfAssignment":"Inlämningsuppgift3"},{"grade":"VG","nameOfAssignment":"Projekta"}],"lastName":"Sterner","userName":"simste-6"}
          ],
         selected_kursKod: '',
-        selected_modul: ''
+        selected_modul: '',
+        selected_items: [],
+        grades_options: ['U', 'G', 'VG']
       }
 
     },
@@ -60,7 +97,10 @@ export default {
     submitKursKod(){
       this.URL_kursKod = "" + this.selected_kursKod
       console.log(this.URL_kursKod)
-    }
+    },
+    onRowSelected(items) {
+        this.selected_items = items
+      }
   }
 }
 </script>
